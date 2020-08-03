a3b04cc089a48a3c9c9c7a0f00b229ce6b8b9c1c
Tate <>
2020-08-03 18:06:56 +0800

    数组模拟队列++

---
+++ 数组模拟队列.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var xx =  ArrayQueue(maxSize: 30)
+
+class ArrayQueue{
+    var maxSize:Int = 0
+    var front:Int = 0
+    var rear:Int = 0
+    var arr:[Int] = []
+    
+     init(maxSize : Int) {
+        self.maxSize = maxSize
+        print("12312321")
+    }
+    
+}
+
+
+
