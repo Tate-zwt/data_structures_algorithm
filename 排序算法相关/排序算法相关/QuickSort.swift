3ec494f8944b632afcaa900ad06e3f01620cd73d
Tate <>
2020-08-09 22:50:38 +0800

    代码排版

--- 排序算法相关/排序算法相关/QuickSort.swift
+++ 排序算法相关/排序算法相关/QuickSort.swift
@@ -20,12 +20,10 @@ func quickSort(_ arr: inout [Int],left:Int,right:Int) {
                     //在 pivot 的左边一直找,找到大于等于 pivot 值,才退出
                     while arr[l] < pivot {
                         l += 1
-            
                     }
                     //在 pivot 的右边一直找,找到小于等于 pivot 值,才退出
                     while(arr[r] > pivot) {
                         r -= 1
-                
                     }
                     //如果 l >= r 说明 pivot 的左右两的值，已经按照左边全部是
                     //小于等于 pivot 值，右边全部是大于等于 pivot 值
@@ -39,33 +37,24 @@ func quickSort(_ arr: inout [Int],left:Int,right:Int) {
                     //如果交换完后，发现这个 arr[l] == pivot 值 相等 r--， 前移
                     if arr[l] == pivot {
                         r -= 1
-                        
                     }
                     //如果交换完后，发现这个 arr[r] == pivot 值 相等 l++， 后移
                     if arr[r] == pivot  {
                         l += 1
-                        
                     }
                 }
-                // 如果 l == r, 必须 l++, r--, 否则为出现栈溢出
-                if l == r {
-                    l += 1
-                    r -= 1
-                    
-                }
-
-            //向左递归
-            if left < r {
-               quickSort(&arr, left:left, right:r)
-                
-            }
-            //向右递归
-            if right > l {
-              quickSort(&arr, left:l, right:right)
-                
-            }
-    
-   
-    
+        // 如果 l == r, 必须 l++, r--, 否则为出现栈溢出
+        if l == r {
+            l += 1
+            r -= 1
+        }
+        //向左递归
+        if left < r {
+           quickSort(&arr, left:left, right:r)
+        }
+        //向右递归
+        if right > l {
+          quickSort(&arr, left:l, right:right)
+        }
 }
 
