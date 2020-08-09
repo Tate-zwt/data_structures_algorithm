398f07860746f5d73781d6fd6f1af100b392e04c
Tate <>
2020-08-09 15:20:13 +0800

    冒泡排序

---
+++ Bubble Sort(冒泡排序).playground/Contents.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+//常规冒泡排序
+func bubbleSort(_ numbers:[Int]) -> [Int]{
+    //改成可变数组
+    var nums = numbers
+//    时间复杂度 O(n^2)
+    let n = nums.count
+    for i in 0..<(n - 1)  {
+        for j in 0..<(n - 1 - i) {
+            //如果前面的数比后面的数大就交换
+            if nums[j] > nums[j + 1] {
+                nums.swapAt(j, j + 1)
+//                let temp = result[i]
+//                result[i] = result[j]
+//                result[j] = temp
+            }
+        
+        }
+        print("第\(i + 1)趟排序后的数组")
+        print(nums)
+        
+    }
+    return nums
+
+}
+print("常规冒泡排序是 --- \(bubbleSort([3,9,-1,10,20]))")
+
+
+
+//优化后的冒泡排序
+func bubbleSortOptimize(_ numbers:[Int]) -> [Int]{
+    //改成可变数组
+    var nums = numbers
+//    时间复杂度 O(n^2)
+    let n = nums.count
+    
+    var flag = false //标识变量，标识是否进行交换过，优化算法
+    
+    for i in 0..<(n - 1)  {
+        for j in 0..<(n - 1 - i) {
+            
+            //如果前面的数比后面的数大就交换
+            if nums[j] > nums[j + 1] {
+                flag = true
+                nums.swapAt(j, j + 1)
+//                let temp = result[i]
+//                result[i] = result[j]
+//                result[j] = temp
+            }
+        }
+        
+        print("第\(i + 1)趟排序后的数组")
+        print(nums)
+        
+        if !flag { //在一趟排序中，一次交换都没有发生过
+            break
+        }else{
+            flag = false // 重置变量，进行下次判断
+        }
+    }
+    return nums
+
+}
+
+print("优化后的冒泡排序是 --- \(bubbleSortOptimize([3,9,-1,10,20]))")
+
+
--- 链表相关/链表相关/Stack.swift
+++ 链表相关/链表相关/Stack.swift
@@ -10,15 +10,31 @@ import Foundation
 //swift 栈的实现
 struct Stack<Element> {
     var items = [Element]()
+    //入栈
     mutating func push(_ item:Element) {
+        if isFull() {
+            print("栈满")
+        }
         items.append(item)
     }
+    //出栈
     mutating func pop() ->Element? {
+        if isEmpty() {
+            print("栈空")
+            return nil
+        }
         return items.removeLast()
     }
-    func empty() -> Bool {
+    //栈空
+    func isEmpty() -> Bool {
         return items.isEmpty
     }
+    
+    //栈满
+    func isFull() -> Bool {
+        return length() == items.count
+    }
+    
     func peek() ->Element? {
         return items.last
     }
