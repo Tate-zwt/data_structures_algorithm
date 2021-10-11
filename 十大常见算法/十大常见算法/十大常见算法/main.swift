814dee9cfef404f9f5213b8fc2f67c874f6b446f
Tate <>
2021-10-11 23:00:48 +0800

    分治算法

---
+++ 十大常见算法/十大常见算法/十大常见算法/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  十大常见算法
+//
+//  Created by Tate on 2021/10/11.
+//
+
+import Foundation
+
+//print("二分查找算法")
+//let nums = [1,8, 10, 89, 1000, 1234]
+////二分查找算法
+////let index =  binarySearch(nums, 1000, leftIndex: 0, rightIndex: nums.count - 1)
+////print("二分查找索引为 ---- \(index)")
+//
+//let index =  binarySearch3(nums, 8)
+//print("非递归二分查找索引为 ---- \(index)")
+
+
+//分治算法
+hanoiTower(num: 10, a:"A" , b: "B", c:"C" )
